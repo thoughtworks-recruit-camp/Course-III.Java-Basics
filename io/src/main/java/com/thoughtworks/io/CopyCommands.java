0a283620c9826f087fa7126299ed947c2d1b3460
Zhu Mei <meizhu812@outlook.com>
2020-02-24 15:40:20 +0800

    III.4-fix posix command (final)

--- io/src/main/java/com/thoughtworks/io/CopyCommands.java
+++ io/src/main/java/com/thoughtworks/io/CopyCommands.java
@@ -3,7 +3,7 @@ package com.thoughtworks.io;
 public enum CopyCommands {
     WinModern("cmd /c robocopy {0} {1} /E"),
     WinLegacy("cmd /c xcopy {0} {1} /E"),
-    Posix("mkdir -p {1} && cp -rdp {0} {1}");
+    Posix("cp -r {0}/. {1}");
     public final String command;
 
     CopyCommands(String commandString) {
