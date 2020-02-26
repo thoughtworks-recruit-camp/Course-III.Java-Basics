4ed5422168279343de54cce0a2225659a3023429
Zhu Mei <meizhu812@outlook.com>
2020-02-26 21:30:47 +0800

    III.4-fix

--- io/src/main/java/com/thoughtworks/io/CopyCommands.java
+++ io/src/main/java/com/thoughtworks/io/CopyCommands.java
@@ -3,7 +3,7 @@ package com.thoughtworks.io;
 public enum CopyCommands {
     WinModern("cmd /c robocopy {0} {1} /MIR"),
     WinLegacy("cmd /c rmdir /S /Q {1} & xcopy {0} {1} /E"),
-    Posix("rm -rf {1} && cp -r {0}/. {1}");
+    Posix("rm -rf {1} && cp -r {0} {1}");
     public final String command;
 
     CopyCommands(String commandString) {
