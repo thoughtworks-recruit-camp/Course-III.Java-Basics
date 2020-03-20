0cf62ddf713f40a147d8c919b9dd57f8b762f703
Zhu Mei <meizhu812@outlook.com>
2020-03-20 10:11:40 +0800

    Fix Share-2

--- src/main/java/com/thoughtworks/collection/Flatten.java
+++ src/main/java/com/thoughtworks/collection/Flatten.java
@@ -7,23 +7,20 @@ import java.util.stream.Collectors;
 public class Flatten {
 
     Integer[][] array;
+
     public Flatten(Integer[][] array) {
         this.array = array;
     }
 
     public List<Integer> transformToOneDimensional() {
         return Arrays.stream(array)
-                .flatMapToInt(intArr -> Arrays.stream(intArr)
-                        .mapToInt(Integer::intValue))
-                .boxed()
+                .flatMap(Arrays::stream)
                 .collect(Collectors.toList());
     }
 
     public List<Integer> transformToUnrepeatedOneDimensional() {
         return Arrays.stream(array)
-                .flatMapToInt(integers -> Arrays.stream(integers)
-                        .mapToInt(Integer::intValue))
-                .boxed()
+                .flatMap(Arrays::stream)
                 .distinct()
                 .collect(Collectors.toList());
     }
